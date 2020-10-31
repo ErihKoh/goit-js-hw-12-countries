b1793e086a17d2393b7180f000e294d6f1e33f04
Yurii Zubar <barsuk0831@gmail.com>
2020-10-31 12:16:23 +0200

    err

--- src/data/fetchCountries.js
+++ src/data/fetchCountries.js
@@ -3,7 +3,7 @@ const BASE_URL = 'https://restcountries.eu/rest/v2/name';
 
 
     function fetchCountries(sourchName) {
-        return fetch(`${BASE_URL}/${sourchName}`).then(r => r.json(),);
+        return fetch(`${BASE_URL}/${sourchName}`).then(r => r.json());
     }
 
 
