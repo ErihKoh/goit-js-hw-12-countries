fcb01a525bd7ea8610ea914e3039c04ceb94726e
Yurii Zubar <barsuk0831@gmail.com>
2020-10-31 11:46:18 +0200

    end

--- src/data/countries.js
+++ src/data/countries.js
@@ -2,47 +2,67 @@ import API from './fetchCountries';
 import refs from './refs';
 import setCountries from '../templates/list_countries.hbs';
 import oneCountries from '../templates/one_country.hbs';
-// import '@pnotify/core/dist/BrightTheme.css';
+import '@pnotify/core/dist/BrightTheme.css';
 import '@pnotify/core/dist/PNotify.css';
 import { error } from '@pnotify/core';
 import { defaults } from '@pnotify/core';
 
+const debounce = require('lodash.debounce');
+
 defaults.delay = 2000;
 
-const debounce = require('lodash.debounce');
+
 
 refs.searchForm.addEventListener('input', debounce(onSearch, 500));
 
 function onSearch(e) {
     e.preventDefault();
     const sourchName = e.target.value;
+     cleanMarkup();
     
 
     API.fetchCountries(sourchName).then(data => {
+        
         if (data.length <= 10 && data.length > 1) {
-            cleanMarkup();
-            return refs.listCountries.insertAdjacentHTML('beforeend', setCountries(data));
+            // cleanMarkup();
+            const markup = setCountries(data);
+            return createMarkup(markup);
         }
         if (data.length === 1) {
-            cleanMarkup();
+            // cleanMarkup();
             const markup = data.map(({ name, capital, population, languages, flag }) =>
                 oneCountries({ name, capital, population, languages, flag }))
-            return refs.listCountries.insertAdjacentHTML('beforeend', markup)
+            return createMarkup(markup);
         }
         if (data.length > 10) {
-            refs.listCountries.innerHTML = '';
-           error({
+            // cleanMarkup();
+         return  error({
               text: "Too many matches found. Please enter a more specific query!"
           });
         }
+        if (data.status === 404) {
+           return  error({
+                text: "No country has been found. Please enter a more specific query!"
+            });
+        }
         
     
-    }).catch(error => {
-        cleanMarkup();
-        console.log('введите название страны')
+    }).catch(err => {
+        // cleanMarkup();
+        return error({
+                text: "Please enter a query!"
+         });
+        
+        
     });
 }
 
 function cleanMarkup() {
     refs.listCountries.innerHTML = '';
 }
+
+
+function createMarkup(markup) {
+refs.listCountries.insertAdjacentHTML('beforeend', markup);
+};
+ 
